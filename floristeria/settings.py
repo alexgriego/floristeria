318e8a58acb452a2bb87a8f5cfbaed7cb356bbd5
alexander <alexgriego15@hotmail.com>
2021-06-07 17:33:59 -0500

    Comfiguracion para Heroku

--- floristeria/settings.py
+++ floristeria/settings.py
@@ -1,3 +1,5 @@
+import os
+import django_heroku
 """
 Django settings for floristeria project.
 
@@ -124,3 +126,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+django_heroku.settings(locals())
