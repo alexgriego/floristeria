0c191c414da345181cadce979ec0acc8383b6b04
alexander <alexgriego15@hotmail.com>
2021-06-07 17:14:46 -0500

    Se agregaron las tablas Inventario de flores, clientes, Compras y Ofertas

--- flores/admin.py
+++ flores/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from .models import Oferta, Compra, Cliente, Inventario_de_flores
+
+class OfertaAdmin(admin.ModelAdmin):
+    list_display = ('flores_en_descuento', 'valor_de_descuento', 'usuario_con_descuento')
+
+class CompraAdmin(admin.ModelAdmin):
+    list_display = ('valor_de_compra', 'fecha', 'numero_de_factura')
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'documento', 'direccion')
+
+class Inventario_de_floresAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'tipo', 'nit', 'temporada', 'ocasion')
+
 # Register your models here.
+admin.site.register(Oferta, OfertaAdmin)
+admin.site.register(Compra, CompraAdmin)
+admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(Inventario_de_flores, Inventario_de_floresAdmin)
--- flores/models.py
+++ flores/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Inventario_de_flores(models.Model):
+    nombre = models.CharField(max_length=64)
+    tipo = models.CharField(max_length=64)
+    nit = models.CharField(max_length=64)
+    temporada = models.CharField(max_length=64)
+    ocasion = models.CharField(max_length=64)
+    def __str__(self):
+        return self.nombre
+
+class Oferta(models.Model):
+    flores_en_descuento = models.ForeignKey(Inventario_de_flores, on_delete=models.PROTECT)
+    usuario_con_descuento = models.CharField(max_length=64)
+    valor_de_descuento = models.FloatField()
+    def __str__(self):
+        return str (self.flores_en_descuento)
+
+
+class Compra(models.Model):
+    valor_de_compra = models.FloatField()
+    fecha = models.DateTimeField()
+    numero_de_factura = models.FloatField()
+    def __str__(self):
+        return str (self.numero_de_factura)
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=64)
+    documento = models.CharField(max_length=64)
+    direccion = models.CharField(max_length=64)
+    correo = models.EmailField(max_length=64)
+    tarjeta_de_credito = models.CharField(max_length=64)
+    def __str__(self):
+        return self.nombre
